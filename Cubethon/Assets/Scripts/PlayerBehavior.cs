a09725ffe500ea8ec4991178d9daa81b226554ef
Joseph Neubert <joeyfneubert@gmail.com>
2024-04-28 19:10:42 -0700

    Added movement and camera to player

--- Cubethon/Assets/Scripts/PlayerBehavior.cs
+++ Cubethon/Assets/Scripts/PlayerBehavior.cs
@@ -7,6 +7,10 @@ public class PlayerBehavior : MonoBehaviour
     // Declare the Rigidbody component for the physics system
     public Rigidbody _rb;
 
+    // Player variables
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,15 @@ public class PlayerBehavior : MonoBehaviour
     // Update for physics system
     void FixedUpdate()
     {
-        _rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        _rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            _rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+        else if(Input.GetKey("a"))
+        {
+            _rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
     }
 }
